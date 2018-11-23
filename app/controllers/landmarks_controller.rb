2fd9112a46a5f4bb541e620a462d7d3f3fd804d1
Nikki A <nikki.apostolakis@gmail.com>
2018-11-23 22:29:41 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,7 +1,7 @@
 class LandmarksController < ApplicationController
   get '/landmarks' do
     @landmarks = Landmark.all
-    @figures = Figure.all
+    #@figures = Figure.all
     erb :'landmarks/index'
   end
 
