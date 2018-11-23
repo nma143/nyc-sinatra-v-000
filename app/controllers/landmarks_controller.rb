3b896c9176961f8f0abeec67a36e30b04f279b68
Nikki A <nikki.apostolakis@gmail.com>
2018-11-23 22:28:51 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,4 +8,10 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :"landmarks/new"
   end
+
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    #redirect '/landmarks'
+  end
+  
 end
