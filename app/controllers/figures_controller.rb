75701729cfabc12187de2d2651d7c6af68b24ed6
Nikki A <nikki.apostolakis@gmail.com>
2018-11-23 21:41:31 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,8 @@
 class FiguresController < ApplicationController
-
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
   get '/figures/new' do
     erb :'figures/new'
   end
