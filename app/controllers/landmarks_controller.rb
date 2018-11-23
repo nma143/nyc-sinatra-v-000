963f203984a01739452654a02d27cc92d219e0e7
Nikki A <nikki.apostolakis@gmail.com>
2018-11-23 22:29:33 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,9 +1,9 @@
 class LandmarksController < ApplicationController
-  #get '/landmarks' do
-  #  @landmarks = Landmark.all
-  #  @figures = Figure.all
-  #  erb :'landmarks/index'
-  #end
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/index'
+  end
 
   get '/landmarks/new' do
     erb :"landmarks/new"
@@ -11,7 +11,7 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
-    #redirect '/landmarks'
+    redirect '/landmarks'
   end
 
 end
