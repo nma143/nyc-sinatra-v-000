1379277e93076710550c1d2feaf79ca78d047bab
Nikki A <nikki.apostolakis@gmail.com>
2018-11-23 21:40:36 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,4 +4,17 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
+  post '/figures' do
+    @figure = Figure.create(params['figure'])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    #redirect to "/figures/#{@figure.id}"
+  end
 end
